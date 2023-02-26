b9081afb0e096f8db5b385bd4170fa31b4fa3fb2
Jay_d <10457987+jay-d@user.noreply.gitee.com>
2023-02-26 19:22:21 +0800

    1

---
+++ GH3/GH3.h
@@ -0,0 +1,32 @@
+﻿
+// GH3.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CGH3App:
+// 有关此类的实现，请参阅 GH3.cpp
+//
+
+class CGH3App : public CWinApp
+{
+public:
+	CGH3App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CGH3App theApp;
