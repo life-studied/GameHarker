6749cef02d67dfa084720029dd1816d07c858ac5
Jay_d <10457987+jay-d@user.noreply.gitee.com>
2023-02-27 20:26:39 +0800

    v2 over

--- GH3/Resource.h
+++ GH3/Resource.h
@@ -12,6 +12,9 @@
 #define IDC_TAB1                        1001
 #define IDC_LIST1                       1003
 #define IDC_BUTTON1                     1004
+#define IDC_CHECK1                      1005
+#define IDC_CHECK2                      1006
+#define IDC_CHECK3                      1007
 
 // Next default values for new objects
 // 
@@ -19,7 +22,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        134
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
